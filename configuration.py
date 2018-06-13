198dd5972a2942ddecb32727b8329c4388da5995
Milos Vasic <milos85vasic@gmail.com>
2018-06-13 19:00:03 +0200

    Apache Factory Toolkit: System configuration.

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+default_port = 8080
+default_port_mysql = 3306
+account_json = "account.json"
+apache_factory_group = "apache_factory"
+apache_factory_configuration_dir = "/usr/share/apache_factory"
+default_configuration_json = apache_factory_configuration_dir + "/global_configuration.json"
