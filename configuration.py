43e40b5936fbbe00070faa4af5559b22ddc0d46f
Milos Vasic <milos85vasic@gmail.com>
2018-06-13 20:32:08 +0200

    Apache Factory Toolkit: MySQL common.

--- configuration.py
+++ configuration.py
@@ -1,3 +1,17 @@
+apache_factory = "Apache-Factory"
+
+php = "Php"
+mysql = "MySQL"
+apache2 = "Apache2"
+mysql_data_dir = "Data"
+mysql_log_dir = "Log"
+mysql_tmp_dir = "Tmp"
+mysql_sock_dir = "Sock"
+mysql_pid_dir = "Pid"
+mysql_share_dir = "Share"
+mysql_conf_dir = "Conf"
+mysql_installation_dir = "Release"
+
 default_port = 8080
 default_port_mysql = 3306
 account_json = "account.json"
@@ -7,6 +21,10 @@ default_configuration_json = apache_factory_configuration_dir + "/global_configu
 
 wipe_script = "wipe.py"
 content_dir_name = "Content"
+
+
+def get_home_directory_path(account):
+    return "/home/" + account
 
 
 def content_dir_path(home_path):
---
+++ mysql_common.py
@@ -0,0 +1,34 @@
+import sys
+from commands import *
+from configuration import *
+from system_configuration import *
+
+
+def user_home():
+    return get_home_directory_path(account)
+
+
+def get_mysql_bin_directory():
+    return user_home() + "/" + mysql + "/" + mysql_installation_dir + "/usr/local/mysql/bin"
+
+
+def get_mysql_logs_directory():
+    return user_home() + "/" + mysql + "/" + mysql_log_dir
+
+
+def get_start_command(account_home):
+    return "/mysqld --defaults-extra-file=" + account_home + "/" + mysql + "/" + mysql_conf_dir + "/my.conf &"
+
+
+def get_start_command_init(account_home):
+    return "/mysqld --defaults-extra-file=" + account_home + "/" + mysql + "/" + mysql_conf_dir \
+           + "/my.conf --init-file=" + account_home + "/" + apache_factory + "/" + mysql_init_tmp + " &"
+
+
+account = sys.argv[1]
+mysql_init_tmp = "init.tmp"
+
+system_configuration = get_system_configuration()
+
+initialize = "/mysqld --defaults-file=" + user_home() + "/" + mysql + "/" + mysql_conf_dir + \
+             "/my.conf --initialize --user=" + account
