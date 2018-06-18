39d9a93df42bfa186fbea338c96ce7a3cf3ee674
Milos Vasic <milos85vasic@gmail.com>
2018-06-18 16:03:17 +0200

    Refactoring.

--- mysql_common.py
+++ mysql_common.py
@@ -1,7 +1,6 @@
 import sys
 from commands import *
-from configuration import *
-from system_configuration import *
+from mysql_common_5560 import *
 
 
 def user_home():
@@ -26,19 +25,6 @@ def get_start_command(account_home):
 def get_start_command_init(account_home):
     return "/mysqld --defaults-extra-file=" + account_home + "/" + mysql + "/" + mysql_conf_dir \
            + "/my.cnf --init-file=" + account_home + "/" + apache_factory + "/" + mysql_init_tmp + " &"
-
-
-# MySQL 5.5.60:
-def get_mysql_start_command(user_account):
-    mysql_full_path = get_home_directory_path(user_account) + "/" + mysql + "/"
-    port = default_port_mysql
-    if user_account in system_configuration:
-        if key_configuration_port_mysql in system_configuration[user_account]:
-            port = system_configuration[user_account][key_configuration_port_mysql]
-
-    return mysql_full_path + mysql_bin_dir + "/mysqld --tmpdir=" + mysql_full_path + "tmp --datadir=" \
-           + mysql_full_path + "data " + "--secure-file-priv=" + mysql_full_path + "priv --port=" + str(port) \
-           + " --user=" + user_account + " " + "--socket=" + mysql_full_path + "socket &"
 
 
 account = sys.argv[1]
