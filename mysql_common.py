3ec1ba2f0ab44b25d047f77712419ab6c9d2f708
Milos Vasic <milos85vasic@gmail.com>
2018-06-18 15:57:17 +0200

    Refactoring.

--- mysql_common.py
+++ mysql_common.py
@@ -28,23 +28,25 @@ def get_start_command_init(account_home):
            + "/my.cnf --init-file=" + account_home + "/" + apache_factory + "/" + mysql_init_tmp + " &"
 
 
+# MySQL 5.5.60:
+def get_mysql_start_command(user_account):
+    mysql_full_path = get_home_directory_path(user_account) + "/" + mysql + "/"
+    port = default_port_mysql
+    if user_account in system_configuration:
+        if key_configuration_port_mysql in system_configuration[user_account]:
+            port = system_configuration[user_account][key_configuration_port_mysql]
+
+    return mysql_full_path + mysql_bin_dir + "/mysqld --tmpdir=" + mysql_full_path + "tmp --datadir=" \
+           + mysql_full_path + "data " + "--secure-file-priv=" + mysql_full_path + "priv --port=" + str(port) \
+           + " --user=" + user_account + " " + "--socket=" + mysql_full_path + "socket &"
+
+
 account = sys.argv[1]
 mysql_init_tmp = "init.tmp"
 
 system_configuration = get_system_configuration()
 
+# MySQL 8.0:
 initialize = "/mysqld --defaults-file=" + user_home() + "/" + mysql + "/" + mysql_conf_dir + \
              "/my.cnf --initialize --user=" + account
 
-# MySQL 5.5.60:
-port = default_port_mysql
-if account in system_configuration:
-    if key_configuration_port_mysql in system_configuration[account]:
-        port = system_configuration[account][key_configuration_port_mysql]
-
-mysql_full_path = get_home_directory_path(account) + "/" + mysql + "/"
-
-start_mysql_command = mysql_full_path + mysql_bin_dir + "/mysqld --tmpdir=" + mysql_full_path + "tmp --datadir=" + \
-                      mysql_full_path + "data " + "--secure-file-priv=" + mysql_full_path + "priv --port=" + str(port) \
-                      + " --user=" + account + " " + "--socket=" + mysql_full_path + "socket &"
-# MySQL 5.5.60 - end:
