79c6ee7e61e292c85f03f543923a709ec4b60894
Milos Vasic <milos85vasic@gmail.com>
2018-07-23 15:55:02 +0200

    Pyramid Factory revert.

--- configuration.py
+++ configuration.py
@@ -22,9 +22,7 @@ default_port = 8080
 default_port_mysql = 3306
 account_json = "account.json"
 apache_factory_group = "apache_factory"
-pyramid_factory_group = "pyramid_factory"
 apache_factory_configuration_dir = "/usr/share/apache_factory"
-pyramid_factory_configuration_dir = "/usr/share/pyramid_factory"
 default_configuration_json = apache_factory_configuration_dir + "/global_configuration.json"
 
 wipe_script = "wipe.py"
