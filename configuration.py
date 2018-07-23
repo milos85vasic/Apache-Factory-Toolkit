3e061b193c9b5f0f176b84f6957165d26342308e
Milos Vasic <milos85vasic@gmail.com>
2018-07-23 15:52:34 +0200

    Pyramid Factory directory.

--- configuration.py
+++ configuration.py
@@ -24,6 +24,7 @@ account_json = "account.json"
 apache_factory_group = "apache_factory"
 pyramid_factory_group = "pyramid_factory"
 apache_factory_configuration_dir = "/usr/share/apache_factory"
+pyramid_factory_configuration_dir = "/usr/share/pyramid_factory"
 default_configuration_json = apache_factory_configuration_dir + "/global_configuration.json"
 
 wipe_script = "wipe.py"
