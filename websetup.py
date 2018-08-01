e47a33b8dfb9624bc2847101db56f5e42e71cfb1
Milos Vasic <milos85vasic@gmail.com>
2018-08-01 16:51:01 +0200

    Pyramid Factory: Web setup.

--- websetup.py
+++ websetup.py
@@ -7,12 +7,13 @@ what = sys.argv[1]
 if len(sys.argv) >= 3:
     branch = sys.argv[2]
 
-setup = "python websetup_run.py " + what
+setup = "python ./Toolkit/websetup_run.py " + what
 if branch is not "master":
     setup += " " + branch
 
 steps = [
-    "git clone --recurse-submodules https://github.com/milos85vasic/Apache-Factory-Toolkit.git ./",
+    "mkdir Toolkit",
+    "git clone --recurse-submodules https://github.com/milos85vasic/Apache-Factory-Toolkit.git ./Toolkit",
     setup
 ]
 
