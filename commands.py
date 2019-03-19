e19f4fa3396053426728995d5ccef0669c771cd8
Milos Vasic <milos85vasic@gmail.com>
2019-03-19 10:49:53 +0100

    Apache Factory: Fixing and polishing.

--- commands.py
+++ commands.py
@@ -142,7 +142,7 @@ def git_checkout(what):
 
 
 def python(script, *params):
-    if os.path.isfile(script):
+    if os.path.exists(script):
         arguments = ""
         for item in params:
             arguments += " " + item
