c34159ea1a82b9b8ba8165922221cce2266e0a1d
Milos Vasic <milos85vasic@gmail.com>
2020-01-03 11:38:40 +0100

    MSF-2 - Utf-8 conversion.

--- git_info.py
+++ git_info.py
@@ -17,15 +17,17 @@ def set_git_info():
     branch_split_result = branch_result.splitlines(keepends=False)
 
     for line in url_split_result:
-        print("> > > ", line)
-        if fetch_url in str(line):
-            repository = str(line).replace(fetch_url, "").strip()
+        utf_line = line.decode('UTF-8')
+        print("> > > ", utf_line)
+        if fetch_url in utf_line:
+            repository = utf_line.replace(fetch_url, "").strip()
             break
 
     for line in branch_split_result:
-        print("> > > ", line)
-        if "* " in str(line):
-            branch = str(line)[str(line).index(star) + star.__len__():].strip()
+        utf_line = line.decode('UTF-8')
+        print("> > > ", utf_line)
+        if "* " in utf_line:
+            branch = utf_line[utf_line.index(star) + star.__len__():].strip()
             break
 
     git_configuration = {
