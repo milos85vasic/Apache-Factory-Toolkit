955d0e3faf4c410c83126b2cb4b58bb3f9468406
Milos Vasic <milos85vasic@gmail.com>
2020-01-09 12:40:13 +0100

    Debugging.

--- websetup.py
+++ websetup.py
@@ -16,23 +16,26 @@ def get_python_cmd():
     return "python"
 
 
-branch = "master"
-what = sys.argv[1]
+if __name__ == '__main__':
+    branch = "master"
+    what = sys.argv[1]
 
-if len(sys.argv) >= 3:
-    branch = sys.argv[2]
+    if len(sys.argv) >= 3:
+        branch = sys.argv[2]
 
-setup = get_python_cmd() + " ./Toolkit/websetup_run.py " + what
-if branch is not "master":
-    setup += " " + branch
+    python_cmd = get_python_cmd()
+    print("Python cmd: ", python_cmd)
+    setup = python_cmd + " ./Toolkit/websetup_run.py " + what
+    if branch is not "master":
+        setup += " " + branch
 
-steps = [
-    "mkdir Toolkit",
-    "git clone --recurse-submodules https://github.com/milos85vasic/Apache-Factory-Toolkit.git ./Toolkit",
-    setup,
-    "rm -rf ./Toolkit",
-    "rm -f " + os.path.basename(__file__)
-]
+    steps = [
+        "mkdir Toolkit",
+        "git clone --recurse-submodules https://github.com/milos85vasic/Apache-Factory-Toolkit.git ./Toolkit",
+        setup,
+        "rm -rf ./Toolkit",
+        "rm -f " + os.path.basename(__file__)
+    ]
 
-for cmd in steps:
-    os.system(cmd)
+    for cmd in steps:
+        os.system(cmd)
