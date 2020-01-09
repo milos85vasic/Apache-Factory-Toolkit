d1a16420e0f4c527f0d58bb50a0bbada53e31eb6
Milos Vasic <milos85vasic@gmail.com>
2020-01-09 12:54:53 +0100

    Work in progress.

--- echo_python_cmd.py
+++ echo_python_cmd.py
@@ -11,21 +11,11 @@ if __name__ == '__main__':
     for cmd in steps:
         os.system(cmd)
 
-    branch = "master"
-    what = sys.argv[1]
-
-    if len(sys.argv) >= 3:
-        branch = sys.argv[2]
-
     from Toolkit.commands import get_python_cmd
     python_cmd = get_python_cmd()
-    print("Python cmd: ", python_cmd)
-    setup = python_cmd + " ./Toolkit/websetup_run.py " + what
-    if branch is not "master":
-        setup += " " + branch
+    print(python_cmd)
 
     steps = [
-        setup,
         "rm -rf ./Toolkit",
         "rm -f " + os.path.basename(__file__)
     ]
