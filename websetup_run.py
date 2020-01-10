0b641372a3d77fd7dd2ff7fc29342616a4c7b019
Milos Vasic <milos85vasic@gmail.com>
2020-01-10 11:22:49 +0100

    MSF-2 - Refactoring.

--- websetup_run.py
+++ websetup_run.py
@@ -1,60 +1,62 @@
 import sys
-from Toolkit.commands import *
-from Toolkit.configuration import *
+from .commands import *
+from .configuration import *
 
 branch = "master"
-what = sys.argv[1]
 
-if len(sys.argv) >= 3:
-    branch = sys.argv[2]
+if __name__ == '__main__':
+    what = sys.argv[1]
 
-if what == apache_factory:
-    steps = [
-        run_as_su(
-            concatenate(
-                cd("/root"),
-                mkdir(apache_factory),
-                cd(apache_factory),
-                git_clone_to_recursive_submodules("https://github.com/milos85vasic/Apache-Factory.git"),
-                git_checkout(branch)
+    if len(sys.argv) >= 3:
+        branch = sys.argv[2]
+
+    if what == apache_factory:
+        steps = [
+            run_as_su(
+                concatenate(
+                    cd("/root"),
+                    mkdir(apache_factory),
+                    cd(apache_factory),
+                    git_clone_to_recursive_submodules("https://github.com/milos85vasic/Apache-Factory.git"),
+                    git_checkout(branch)
+                )
             )
-        )
-    ]
+        ]
 
-    run(steps)
-    exit()
+        run(steps)
+        exit()
 
-if what == pyramid_factory:
-    steps = [
-        run_as_su(
-            concatenate(
-                cd("/root"),
-                mkdir(pyramid_factory),
-                cd(pyramid_factory),
-                git_clone_to_recursive_submodules("https://github.com/milos85vasic/Pyramid-Factory.git"),
-                git_checkout(branch)
+    if what == pyramid_factory:
+        steps = [
+            run_as_su(
+                concatenate(
+                    cd("/root"),
+                    mkdir(pyramid_factory),
+                    cd(pyramid_factory),
+                    git_clone_to_recursive_submodules("https://github.com/milos85vasic/Pyramid-Factory.git"),
+                    git_checkout(branch)
+                )
             )
-        )
-    ]
+        ]
 
-    run(steps)
-    exit()
+        run(steps)
+        exit()
 
-if what == mail_server_factory:
-    steps = [
-        run_as_su(
-            concatenate(
-                cd("/root"),
-                mkdir(mail_server_factory),
-                cd(mail_server_factory),
-                git_clone_to_recursive_submodules("https://github.com/milos85vasic/Mail-Server-Factory"),
-                git_checkout(branch)
+    if what == mail_server_factory:
+        steps = [
+            run_as_su(
+                concatenate(
+                    cd("/root"),
+                    mkdir(mail_server_factory),
+                    cd(mail_server_factory),
+                    git_clone_to_recursive_submodules("https://github.com/milos85vasic/Mail-Server-Factory"),
+                    git_checkout(branch)
+                )
             )
-        )
-    ]
+        ]
 
-    run(steps)
-    exit()
+        run(steps)
+        exit()
 
-print("Not recognized: " + what)
-exit(1)
+    print("Not recognized: " + what)
+    exit(1)
